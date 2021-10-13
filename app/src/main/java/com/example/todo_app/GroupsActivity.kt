a3f469dafea4924ef37b78f50da4027a6d0aefdb
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 17:19:38 +1300

    Added ability to add new Group. It will update in the RecyclerView.

--- app/src/main/java/com/example/todo_app/GroupsActivity.kt
+++ app/src/main/java/com/example/todo_app/GroupsActivity.kt
@@ -2,17 +2,22 @@ package com.example.todo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_app.databinding.GroupsBinding
 
 class GroupsActivity : AppCompatActivity() {
     private lateinit var binding: GroupsBinding
+
+    var groupsAdapter: GroupsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,7 @@ class GroupsActivity : AppCompatActivity() {
         binding.groupsRecyclerView.layoutManager = LinearLayoutManager(this)
 
         AppData.initialize()
-        val groupsAdapter = GroupsAdapter(AppData.groups)
+        groupsAdapter = GroupsAdapter(AppData.groups)
         binding.groupsRecyclerView.adapter = groupsAdapter
     }
     private fun setBindings() {
@@ -30,6 +35,28 @@ class GroupsActivity : AppCompatActivity() {
     }
 
     fun createNewGroup(v: View) {
+        val builder = AlertDialog.Builder(this)
 
+        builder.setTitle("New Group")
+        builder.setMessage("Please enter a name for your new group")
+
+        val myInput = EditText(this)
+        myInput.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(myInput)
+
+        builder.setPositiveButton("Save") { dialog, which ->
+            val groupName: String = myInput.text.toString()
+            val newGroup = Group(groupName, mutableListOf())
+
+            AppData.groups.add(newGroup)
+            groupsAdapter!!.notifyItemInserted(AppData.groups.count())
+        }
+
+        builder.setNegativeButton("Cancel") { _, _ ->
+
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
