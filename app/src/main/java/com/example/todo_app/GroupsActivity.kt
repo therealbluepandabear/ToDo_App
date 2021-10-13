03945c794a29451d4376217c8b252b1c32364cc0
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 14:49:01 +1300

    Renamed files.

--- app/src/main/java/com/example/todo_app/GroupsActivity.kt
+++ app/src/main/java/com/example/todo_app/GroupsActivity.kt
@@ -2,17 +2,17 @@ package com.example.todo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.todo_app.databinding.ActivityMainBinding
+import com.example.todo_app.databinding.GroupsBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class GroupsActivity : AppCompatActivity() {
+    private lateinit var binding: GroupsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
     }
     private fun setBindings() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = GroupsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
