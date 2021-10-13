72d42ffa95b178066195b2f794703da8ff0058b4
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 16:14:52 +1300

    Added code for GroupsViewHolder class. Added method functionality for onCreateViewHolder(), onBindViewHolder(), and getItemCount().

--- app/src/main/java/com/example/todo_app/GroupsActivity.kt
+++ app/src/main/java/com/example/todo_app/GroupsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_app.databinding.GroupsBinding
@@ -32,20 +33,28 @@ class GroupsViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
         R.layout.group_row,
         parent,
         false)) {
+    private var groupNameTextView: TextView = itemView.findViewById(R.id.groupNameTextView)
+    private var groupCountTextView: TextView = itemView.findViewById(R.id.groupCountTextView)
 
+    fun bind(group: Group) {
+        groupNameTextView.text = group.name
+        groupCountTextView.text = "${group.items.count()} items"
+    }
 }
 
 class GroupsAdapter(private val list: List<Group>) : RecyclerView.Adapter<GroupsViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupsViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): GroupsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return GroupsViewHolder(inflater, parent)
     }
 
-    override fun onBindViewHolder(holder: GroupsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: GroupsViewHolder,
+                                  position: Int) {
+        val group = list[position]
+        holder.bind(group)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = list.size
 
 }
