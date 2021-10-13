4eeea7fe0a5b85ca61f2f2fa324436f35b8f8c85
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 14:54:24 +1300

    Added ItemsActivity.kt and items.xml.

---
+++ app/src/main/java/com/example/todo_app/ItemsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.todo_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.items)
+    }
+}
