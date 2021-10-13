6146907ce20f81452ee9a0d854e13d0e984203cb
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 15:46:31 +1300

    Added Group and Item data to AppData.kt class.

--- app/src/main/java/com/example/todo_app/AppData.kt
+++ app/src/main/java/com/example/todo_app/AppData.kt
@@ -1,5 +1,20 @@
 package com.example.todo_app
 
 class AppData {
-    
+    companion object DataHolder {
+        var groups: MutableList<Group> = mutableListOf()
+
+        fun initialize() {
+            val item1 = Item("Bread", false)
+            val item2 = Item("Milk", false)
+
+            val item3 = Item("Tap to cross", false)
+            val item4 = Item("Long press to delete", false)
+
+            val group1 = Group("Home", mutableListOf(item1, item2))
+            val group2 = Group("Training", mutableListOf(item3, item4))
+
+            groups = mutableListOf(group1, group2)
+        }
+    }
 }
