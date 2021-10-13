7802258ab3fb6774b83811bb01160b84f05c15bf
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 14:45:34 +1300

    Added 'setBindings()' function and binding variable of type ActivityMainBinding.

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.todo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBindings()
+    }
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
