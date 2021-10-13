f63093a38fa5ab9875a4d1e42d0d6279f6eb7eb0
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 15:23:55 +1300

    Configured bindings for ItemsActivity.kt.

--- app/src/main/java/com/example/todo_app/ItemsActivity.kt
+++ app/src/main/java/com/example/todo_app/ItemsActivity.kt
@@ -2,10 +2,17 @@ package com.example.todo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo_app.databinding.ItemsBinding
 
 class ItemsActivity : AppCompatActivity() {
+    private lateinit var binding: ItemsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.items)
+    }
+    private fun setBindings() {
+        binding = ItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
