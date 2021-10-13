4899a67ca6c0afd001df20a598216d29f5466f8a
xTheCoderx <justaguyoney@gmail.com>
2021-10-13 15:24:13 +1300

    Called setBindings()

--- app/src/main/java/com/example/todo_app/ItemsActivity.kt
+++ app/src/main/java/com/example/todo_app/ItemsActivity.kt
@@ -9,7 +9,7 @@ class ItemsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.items)
+        setBindings()
     }
     private fun setBindings() {
         binding = ItemsBinding.inflate(layoutInflater)
