b4374ca174e3646e58a6cf4a4d6e1ebe3b90ca26
xTheCoderx <justaguyoney@gmail.com>
2021-10-14 17:33:17 +1300

    Added code for ItemsAdapter.kt and ItemsViewHolder.kt.

--- app/src/main/java/com/example/todo_app/ItemsAdapter.kt
+++ app/src/main/java/com/example/todo_app/ItemsAdapter.kt
@@ -1,4 +1,20 @@
 package com.example.todo_app
 
-class ItemsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter(private val group: Group) : RecyclerView.Adapter<ItemsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ItemsViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
+        val item: Item = group.items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount() = group.items.size
+
 }
--- app/src/main/java/com/example/todo_app/ItemsViewHolder.kt
+++ app/src/main/java/com/example/todo_app/ItemsViewHolder.kt
@@ -1,4 +1,18 @@
 package com.example.todo_app
 
-class ItemsViewHolder {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.item_row, parent, false)) {
+    private var itemNameTextView: TextView = itemView.findViewById(R.id.itemNameTextView)
+    private var itemCheckBox: CheckBox = itemView.findViewById(R.id.itemCheckBox)
+
+    fun bind(item: Item) {
+        itemNameTextView.text = item.name
+        itemCheckBox.isChecked = item.completed
+    }
 }
