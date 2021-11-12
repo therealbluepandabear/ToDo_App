9d81eb4801571a628cdf0da13a9500285cd2ba19
xTheCoderx <justaguyoney@gmail.com>
2021-11-12 18:45:20 +1300

    Added 'totalItems' variable which simply returns 'group.items.size'.

--- app/src/main/java/com/example/todo_app/TaskCompletion.kt
+++ app/src/main/java/com/example/todo_app/TaskCompletion.kt
@@ -1,7 +1,7 @@
 package com.example.todo_app
 
 internal fun findCompletion(group: Group) {
-
+    val totalItems = group.items.size
 }
 
 data class CompletionRate(val activeRate: Float, val completedRate: Float)
