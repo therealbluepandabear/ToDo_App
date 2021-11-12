3c922d3cf63d8db46c8ab52cfdcf03c3f4623b48
xTheCoderx <justaguyoney@gmail.com>
2021-11-12 18:48:38 +1300

    The 'findCompletion' function will now return a 'CompletionRate' object.

--- app/src/main/java/com/example/todo_app/TaskCompletion.kt
+++ app/src/main/java/com/example/todo_app/TaskCompletion.kt
@@ -1,12 +1,14 @@
 package com.example.todo_app
 
-internal fun findCompletion(group: Group) {
+internal fun findCompletion(group: Group): CompletionRate {
     val totalItems = group.items.size
     val activeItems = group.items.count { !it.completed }
     val completedItems = group.items.count { it.completed }
 
-    val activeRate = 100 * activeItems / totalItems
-    val completedRate = 100 * completedItems / totalItems
+    val activeRate = (100 * activeItems / totalItems).toFloat()
+    val completedRate = (100 * completedItems / totalItems).toFloat()
+
+    return CompletionRate(activeRate, completedRate)
 }
 
 data class CompletionRate(val activeRate: Float, val completedRate: Float)
