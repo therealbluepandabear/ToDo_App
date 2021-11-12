60dcb69578b5a81ca81ebc68e1c25ab92fab9fc8
xTheCoderx <justaguyoney@gmail.com>
2021-11-12 18:47:21 +1300

    Added 'activeRate' and 'completedRate' variables.

--- app/src/main/java/com/example/todo_app/TaskCompletion.kt
+++ app/src/main/java/com/example/todo_app/TaskCompletion.kt
@@ -4,6 +4,9 @@ internal fun findCompletion(group: Group) {
     val totalItems = group.items.size
     val activeItems = group.items.count { !it.completed }
     val completedItems = group.items.count { it.completed }
+
+    val activeRate = 100 * activeItems / totalItems
+    val completedRate = 100 * completedItems / totalItems
 }
 
 data class CompletionRate(val activeRate: Float, val completedRate: Float)
