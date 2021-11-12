52936ec8046fcffb1d20117d334420d3f90ba3e7
xTheCoderx <justaguyoney@gmail.com>
2021-11-12 18:46:18 +1300

    Added 'completedItems' variable.

--- app/src/main/java/com/example/todo_app/TaskCompletion.kt
+++ app/src/main/java/com/example/todo_app/TaskCompletion.kt
@@ -3,6 +3,7 @@ package com.example.todo_app
 internal fun findCompletion(group: Group) {
     val totalItems = group.items.size
     val activeItems = group.items.count { !it.completed }
+    val completedItems = group.items.count { it.completed }
 }
 
 data class CompletionRate(val activeRate: Float, val completedRate: Float)
